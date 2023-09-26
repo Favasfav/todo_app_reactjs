565c62fc7439310fd5434376b314bce301b54703
Favasfav <favas.fav@gmail.com>
2023-09-26 15:18:42 +0530

    first compleated

---
+++ sample/sample.js
@@ -0,0 +1,23 @@
+// let person = {
+//     name : 'sheheem',
+//     age : 12,
+//     place : 'malappuram',
+    
+// }
+
+// function sample({name, age, place}){
+//     console.log(name);
+//     console.log(age);
+// }
+
+// sample(person)
+const body=document.body
+const div =document.createElement("div")
+
+body.append(div)
+// div.innerText="<strong>Hai</strong>"
+// div.textContent="hello"
+// div.innerHTML="<strong><i>Hai</i></strong>"
+const hlo=document.querySelector("#helo")
+// hlo.remove()
+// div.getAttribute(id="#hello")
